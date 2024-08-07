32184080533b2acd91980f09e50e1f6dfddccf89
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-07 20:08:28 +0300

    .

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Check if the table exists and drop it if it does
+DROP TABLE IF EXISTS users;
+
+-- Create the users table
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255)
+);
