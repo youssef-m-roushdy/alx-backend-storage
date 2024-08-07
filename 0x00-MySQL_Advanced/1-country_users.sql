47938dc3ce4e8386c4eeab40c71e5b28bd98151b
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-07 20:58:55 +0300

    .

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,14 @@
--- Check if the table exists and drop it if it does
-DROP TABLE IF EXISTS users;
-
--- Create the users table
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') DEFAULT 'US'
-);
+-- Select the 'origin' column and calculate the total number of fans for each origin
+SELECT
+  origin,
+  -- Sum the number of fans for each origin and use COALESCE to handle cases where there might be NULL values (in this case, though, NULL values in 'fans' will be treated as 0)
+  COALESCE(SUM(fans), 0) AS nb_fans
+FROM
+  -- Specify the table from which to retrieve the data
+  metal_bands
+-- Group the results by the 'origin' column to aggregate the fan counts for each unique origin
+GROUP BY
+  origin
+-- Order the results by the total number of fans in descending order, so the origin with the most fans appears first
+ORDER BY
+  nb_fans DESC;
