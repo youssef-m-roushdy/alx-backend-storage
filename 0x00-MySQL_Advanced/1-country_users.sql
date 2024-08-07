f987f99af38541d8b4967594879212a4564e0b79
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-07 21:00:18 +0300

    .

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,14 +1,2 @@
 -- Select the 'origin' column and calculate the total number of fans for each origin
-SELECT
-  origin,
-  -- Sum the number of fans for each origin and use COALESCE to handle cases where there might be NULL values (in this case, though, NULL values in 'fans' will be treated as 0)
-  COALESCE(SUM(fans), 0) AS nb_fans
-FROM
-  -- Specify the table from which to retrieve the data
-  metal_bands
--- Group the results by the 'origin' column to aggregate the fan counts for each unique origin
-GROUP BY
-  origin
--- Order the results by the total number of fans in descending order, so the origin with the most fans appears first
-ORDER BY
-  nb_fans DESC;
+SELECT origin, COALESCE(SUM(fans), 0) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
