d8a089b1cc7108a7421c6c30944db918deb4aad4
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-07 21:02:08 +0300

    .

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,2 +1,5 @@
--- Select the 'origin' column and calculate the total number of fans for each origin
-SELECT origin, COALESCE(SUM(fans), 0) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
+-- Query to get the total number of fans grouped by origin, sorted in descending order
+SELECT origin, COALESCE(SUM(fans), 0) AS nb_fans 
+FROM metal_bands 
+GROUP BY origin 
+ORDER BY nb_fans DESC;
