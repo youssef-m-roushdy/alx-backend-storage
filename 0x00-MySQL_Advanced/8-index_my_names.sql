09f19c7b75800172b133365142931e7beec4bd32
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-08 21:17:19 +0300

    .

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -2,4 +2,4 @@
 -- column in the 'names' table. Indexing the 'name' column will improve 
 -- the performance of queries that filter or sort data based on this column.
 
-CREATE INDEX idx_name_first ON names(name);
+CREATE INDEX idx_name_first ON names(name(1));
