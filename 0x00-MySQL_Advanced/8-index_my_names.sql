b8d7ff6c07ff88aab18e69e0d508dead637f38f9
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-08 21:15:51 +0300

    .

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,5 @@
+-- This statement creates an index named 'idx_name_first' on the 'name' 
+-- column in the 'names' table. Indexing the 'name' column will improve 
+-- the performance of queries that filter or sort data based on this column.
+
+CREATE INDEX idx_name_first ON names(name);
