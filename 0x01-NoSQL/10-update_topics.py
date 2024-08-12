b7c37b12cbbc2d35198b6636c2fc5d2e3927c258
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-13 00:09:06 +0300

    .

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,6 +3,7 @@
 Module for updating topics in a MongoDB collection.
 """
 
+
 def update_topics(mongo_collection, name, topics):
     """
     Update the 'topics' field for documents with the specified
