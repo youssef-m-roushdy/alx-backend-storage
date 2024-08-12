ee28382a78b467b27b95d943eac3970177097d40
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-12 23:48:57 +0300

    .

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -7,13 +7,6 @@ Module for inserting a school document into a MongoDB collection.
 def insert_school(mongo_collection, **kwargs):
     """
     Insert a new school document into the MongoDB collection.
-
-    Args:
-        mongo_collection: The MongoDB collection where the document will be inserted.
-        **kwargs: Keyword arguments representing the fields to include in the document.
-
-    Returns:
-        The result of the insert operation.
     """
     document = {
         'name': kwargs.get('name'),
