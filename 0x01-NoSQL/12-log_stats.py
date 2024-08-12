e1380ed929ffc012e17efaeffae9fbabc7079410
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-13 01:00:24 +0300

    .

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,16 +5,25 @@ Script to count and display HTTP request logs from a MongoDB collection.
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
+def main():
+    """
+    Connects to the MongoDB server, retrieves the 'nginx' collection,
+    and prints counts of logs based on HTTP methods and status checks.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
 
-total_logs = nginx_collection.count_documents({})
-print(f"{total_logs} logs")
+    total_logs = nginx_collection.count_documents({})
+    print("{} logs".format(total_logs))
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-for method in methods:
-    count = nginx_collection.count_documents({"method": method})
-    print(f"\tmethod {method}: {count}")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
 
-status_check = nginx_collection.count_documents({"path": "/status"})
-print(f"{status_check} status check")
+    status_check = nginx_collection.count_documents({"path": "/status"})
+    print("{} status check".format(status_check))
+
+
+if __name__ == "__main__":
+    main()
