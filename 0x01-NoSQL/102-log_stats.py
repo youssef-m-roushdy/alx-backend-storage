8104e91a03b52070669b59abaf359d6f79441312
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-14 02:19:10 +0300

    .

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" 12. Log stats
+"""
+
+
+from pymongo import MongoClient
+
+
+def log_stats():
+    """ log_stats.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    ips_present = logs_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$project": {"_id": 0, "ip": "$_id", "count": 1}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}])
+
+    print(f"{total} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
+
+    print("IPS")
+    for ip_present in ips_present:
+        print(f"\t{ip_present.get("ip")}: {ip_present.get("count")}")
+
+
+if __name__ == "__main__":
+    log_stats()
