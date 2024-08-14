6662e02a1cbdabfa82ea24eb2875ddce14df3ac1
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-14 20:55:30 +0300

    .

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,19 +7,12 @@ import redis
 import uuid
 from typing import Union
 
-
 class Cache:
-    """
-        A class to represent a cache system using Redis.
-    """
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, float, bytes, int]) -> str:
-        """
-            Stores the given data in the Redis cache.
-        """
-        user_key = str(uuid.uuid4())
-        self._redis.set(user_key, data)
-        return user_key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
