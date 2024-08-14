855facd588b8c5e2315bda1e170b481ec60cd35a
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-14 22:34:15 +0300

    .

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,7 +5,23 @@
 
 import redis
 import uuid
-from typing import Union, Optional, Callable
+from typing import Union, Optional, Callable, Any
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Tracks the number of calls made to a method in a Cache class.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs) -> Any:
+        """
+        returns the given method after incrementing its call counter.
+        """
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+            return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -13,6 +29,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
