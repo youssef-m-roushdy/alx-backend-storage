93f849ca17dffa0f418321db6a78916257fac7cd
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-14 20:49:52 +0300

    .

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+    This module provides a Cache class to interact with Redis.
+"""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+        A class to represent a cache system using Redis.
+    """
+
+    def __init__(self):
+        """
+            Initializes the Redis client and clears the existing database.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, float, bytes, int]) -> str:
+        """
+            Stores the given data in the Redis cache.
+        """
+        user_key = str(uuid.uuid4())
+        self._redis.set(user_key, data)
+        return user_key
